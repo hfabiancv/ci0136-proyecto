1d9067b5791e56a1c0afcdec562f743aab016e87
Harold Carrillo <HAROLD.CARRILLO@ucr.ac.cr>
2023-11-13 01:24:16 -0600

    Added Wave configuration script and some Waves

---
+++ Assets/Scripts/Character/Enemy/Wave.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+
+[CreateAssetMenu(fileName = "Wave", menuName = "Custom/Wave", order = 1)]
+public class Wave : ScriptableObject
+{
+    [field: SerializeField]
+    private Character[] EnemiesInWave { get; set; }
+    [field: SerializeField]
+    public float TimeBeforeThisWave { get; private set; }
+ 
+    [field: SerializeField]
+    public float AmountToSpawn { get; private set; }
+
+    private Dictionary<string, Character> _idCharacter;
+
+    private void Awake()
+    {
+        _idCharacter = new Dictionary<string, Character>();
+        foreach (var enemy in EnemiesInWave)
+        {
+            _idCharacter.Add(enemy.id, enemy);
+        }
+    }
+
+    public string[] GetEnemyIds()
+    {
+        string[] ids = new string[EnemiesInWave.Length];
+        for (int i = 0; i < EnemiesInWave.Length; i++)
+        {
+            ids[i] = EnemiesInWave[i].id;
+        }   
+        return ids;
+    }
+
+    public int GetEnemiesLength()
+    {
+        return EnemiesInWave.Length;
+    }
+
+    public Character GetEnemyPrefabById(string id)
+    {
+        if (!_idCharacter.TryGetValue(id, out var Enemy))
+        {
+            throw new Exception($"Enemy with id {id} does not exit");
+        }
+
+        return Enemy;
+    }
+}
